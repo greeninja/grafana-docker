84c5025463c58b03302d1456c8b3fa1a2017cf50
greeninja <nick@greeninja.co.uk>
2016-11-15 16:31:52 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 : "${GF_PATHS_LOGS:=/var/log/grafana}"
 : "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
 
-chown -R grafana:grafana "$GF_PATHS_DATA" "$GF_PATHS_LOGS"
-chown -R grafana:grafana /etc/grafana
+# chown -R grafana:grafana "$GF_PATHS_DATA" "$GF_PATHS_LOGS"
+# chown -R grafana:grafana /etc/grafana
 
 if [ ! -z ${GF_AWS_PROFILES+x} ]; then
     mkdir -p ~grafana/.aws/
@@ -26,7 +26,7 @@ if [ ! -z ${GF_AWS_PROFILES+x} ]; then
         fi
     done
 
-    chown grafana:grafana -R ~grafana/.aws
+ #    chown grafana:grafana -R ~grafana/.aws
     chmod 600 ~grafana/.aws/credentials
 fi
 
